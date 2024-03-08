f59b7fb89ff415b34c09a099039ed4b031f732ab
kayoMichael <michaelxkayo@hotmail.com>
2024-03-08 20:39:33 +0900

    remove duplicates in place

---
+++ pointer/remove_duplicates_in_place.py
@@ -0,0 +1,83 @@
+"""
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+    Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+    Return k.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+
+If all assertions pass, then your solution will be accepted.
+"""
+
+# Algorithm: Reader and Writer
+
+
+def remove_duplicates_in_place(nums):
+    writer = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i-1]:
+            nums[writer] = nums[i]
+            writer += 1
+
+
+"""
+Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+
+If all assertions pass, then your solution will be accepted.
+"""
+
+# Algorithm reader and writer method with counter.
+
+
+def remove_duplicates_for_more_than_2(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    writer = 1
+    counter = 0
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]:
+            counter += 1
+        else:
+            counter = 0
+
+        if counter <= 1:
+            nums[writer] = nums[i]
+            writer += 1
+
+    return writer
